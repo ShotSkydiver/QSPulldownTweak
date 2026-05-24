660c4e029c9c9a89244d74cf7a29048d92e19d82
Conner Owen <bigglesworth330@gmail.com>
2026-05-24 01:44:51 -0600

    fix status bar color and change app icon to something non-generic

--- app/src/main/java/me/connerowen/qspulldowntweak/ui/activity/MainActivity.kt
+++ app/src/main/java/me/connerowen/qspulldowntweak/ui/activity/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -49,6 +50,8 @@ private const val REPO_URL = "https://github.com/ShotSkydiver/QSPulldownTweak"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
 
         setContent {
             AppTheme {
